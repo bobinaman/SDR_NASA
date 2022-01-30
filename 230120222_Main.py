fce53efac0b9c672876e2189b9a8d1164b49c5d8
bobinaman <ulises.ortega@estudiantat.upc.edu>
2022-01-30 11:20:35 +0100

    4QAM 1st_release
    falta hace la FFt de las componenetes separadas x_I*cos y quadratura pero ya funciona en teoria

--- 230120222_Main.py
+++ 230120222_Main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import math
 modulation=4 #4QAM
 fs=100000
+fc=1e3
 Ts=1/fs
 Tsig= 50*Ts
 T_pulse=Ts
@@ -17,13 +18,41 @@ numOfBits = int(np.floor(Tsig/T_pulse))
 samplesInBit= int(np.floor(samplesNumber/numOfBits))
 print("bitnumber, samples number,bitSamples", numOfBits,samplesNumber,samplesInBit)
 
+def Set_equal_dimensions_2_IQ(vect1,vect2):
+	##this fuction adds 0 to the shorter array in order to 
+	## have same dimension in both arrays
+	len1 = len(vect1)
+	len2 = len(vect2)
+	if(len1>len2):
+		for i in range(len1-len2):
+			vect2.append(0)
+	if(len2>len1):
+		for i in range(len2-len1):
+			vect2.append(0)
+
+	return(vect1,vect2)
 
 
-def dot_product_array(vect1,vect2):
-	dotProduct=[]
-	for i in range(int(len(vect1))):
-		dotProduct.append(vect1[i]*vect2[i])
-	return dotProduct
+
+
+	return(xI_t,xQ_t)
+
+def operating_array(vect1,vect2,operation):
+	if(operation=="dot product"):
+		dotProduct=[]
+		for i in range(int(len(vect1))):
+			dotProduct.append(vect1[i]*vect2[i])
+		return dotProduct
+	elif(operation=="substraction"):
+		Substraction=[]
+		for i in range(int(len(vect1))):
+			Substraction.append(vect1[i]-vect2[i])
+		return Substraction
+	elif(operation=="sumation"):
+		Sum=[]
+		for i in range(int(len(vect1))):
+			sum.append(vect1[i]+vect2[i])
+		return Sum
 def generate_sinusoidal_envelope_in_bit(sin_periode):
 	x_t_envelope=[]
 	t_envelope=np.linspace(0, T_pulse, int(np.floor(fs/numOfBits)), endpoint=False)
@@ -45,7 +74,7 @@ def generate_envelope_in_bit():
 	B=-2/T_pulse
 	C=1
 	for i in range(samplesInBit):
-		square_x_t = A*t_envelope[i]**2+B*t_envelope[i]+C
+		square_x_t = 1#A*t_envelope[i]**2+B*t_envelope[i]+C
 		x_t_envelope.append(square_x_t)
 
 	print("···········································")
@@ -70,9 +99,9 @@ def generate_signal_BaseBand(x_t_envelope,data,Modulation_M):
 	for i in range(0,numOfBits,modulation_bit_factor):
 		
 		if isInphase:		
-			for m in range(1,modulation_bit_factor):
-				
-				bitValue=data[i+m]
+			for m in range(0,modulation_bit_factor):
+				dataIndex=i
+				bitValue=data[i]
 				#bitValue=2.*bitValue-1
 				for j in range(samplesInBit):
 					if bitValue==0:
@@ -80,12 +109,14 @@ def generate_signal_BaseBand(x_t_envelope,data,Modulation_M):
 					else:
 						xI_t.append(-1 * (x_t_envelope[j]))
 					#we modulate the signal pulse from 1 to -1
+
+				dataIndex = dataIndex + 1
 			isInphase=False
 		else:
 
-			for m in range(1,modulation_bit_factor+1):
-				
-				bitValue=data[i+m]
+			for m in range(0,modulation_bit_factor):
+				dataIndex=i
+				bitValue=data[i]
 				#bitValue=2.*bitValue-1
 				for j in range(samplesInBit):
 					if bitValue==0:
@@ -93,13 +124,25 @@ def generate_signal_BaseBand(x_t_envelope,data,Modulation_M):
 					else:
 						xQ_t.append(-1 * (x_t_envelope[j]))
 					#we modulate the signal pulse from 1 to -1
+				dataIndex = dataIndex + 1
 			isInphase=True
 				
 	return(xI_t,xQ_t)
-def generate_modulated_signal_InphaseANDQuadrature(fc,t,x_t):
-	modulated_Inphase = x_t*math.cos(2*math.pi*fc*t)
-	modulated_Quadrature = x_t*math.sin(2*math.pi*fc*t)
-	modulated_signal = modulated_Inphase - modulated_Quadrature
+def generate_modulated_signal_InphaseANDQuadrature(fc,t,xI_t,xQ_t):
+	
+	[xI_t,xQ_t] = Set_equal_dimensions_2_IQ(xI_t,xQ_t)
+	##esta mal el Tsig hay que cambairlo por el tiempo nuevo tras modular
+	##ya que esto es sin modulacion
+	t_IQ = t = np.linspace(0, Tsig, len(xI_t), endpoint=False)
+	cos=[]
+	sin=[]
+	for i in range(len(t_IQ)):
+		cos.append(math.cos(2*math.pi*fc*t_IQ[i]))
+		sin.append(math.sin(2*math.pi*fc*t_IQ[i]))
+	xIcos=operating_array(xI_t,cos,"dot product")
+	xQsin=operating_array(xQ_t,sin,"dot product")
+	modulated_signal = operating_array(xIcos,xQsin,"substraction")
+	##modulated_signal = modulated_Inphase - modulated_Quadrature
 	return modulated_signal
 #def modulation_constellation():
 
@@ -116,14 +159,18 @@ def plot_modulated_signal_and_FFT(x_t):
 		
 data = initialize_bit_chain()
 T_sin_envelope=T_pulse/2
-[t_envelope,x_t_envelope]=generate_sinusoidal_envelope_in_bit(T_sin_envelope)
+ #[t_envelope,x_t_envelope]=generate_sinusoidal_envelope_in_bit(T_sin_envelope)
 [t_envelope,x_t_envelope]=generate_envelope_in_bit()
-#[xI_t,xQ_t] = generate_signal_BaseBand(x_t_envelope,data,modulation)
-#plot_modulated_signal_and_FFT(xI_t)
-#plot_modulated_signal_and_FFT(xQ_t)
+[xI_t,xQ_t] = generate_signal_BaseBand(x_t_envelope,data,modulation)
+modulated_signal = generate_modulated_signal_InphaseANDQuadrature(fc,t,xI_t,xQ_t)
 
+# plot_modulated_signal_and_FFT(xI_t)
+# plot_modulated_signal_and_FFT(xQ_t)
 
-
+t_IQ = t = np.linspace(0, Tsig, len(xI_t), endpoint=False)
+plt.figure()
+plt.plot(t_IQ,modulated_signal)
+plt.show()
 #autocorrelation1=np.correlate(x_t, x_t)
 #autocorrelation_delay = np.correlate(x_t-T_delay)
 # pwm = signal.square(2 * np.pi * 30 * t, duty=1)
